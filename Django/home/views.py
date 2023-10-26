cc42c815a5f2500b576ff6f9f5c399257d9ceec1
emanuelrzolet <emanuelzolet1@gmail.com>
2023-10-25 22:25:00 -0300

    Adicionado aplicação home

--- Django/home/views.py
+++ Django/home/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return HttpResponse()
+def index(request):
+    return render(request, "homePage/index.html")
