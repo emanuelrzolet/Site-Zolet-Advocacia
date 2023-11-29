3139eb0f6b20377fba009342766476c1a0f5bd24
emanuelrzolet <emanuelzolet1@gmail.com>
2023-11-28 21:40:27 -0300

    Finalizando trabalhos

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -125,5 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
